69e5e1f7b4b31bec53a0593ffbfdcc1f40eb9647
Ahmet Aydın <38251330+HeliosLive@users.noreply.github.com>
2021-03-07 23:11:29 +0100

    search dummy and other smart component tests added

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -5,10 +5,13 @@ import { SharedUIModule } from 'src/libs';
 import { IgxCarouselModule, IgxSliderModule } from 'igniteui-angular';
 import { HomeComponent } from './home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let tagEl: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -32,5 +35,21 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have 5 city cards inside of carousel after initialized', () => {
+    const slides = fixture.debugElement.queryAll(By.css('.actionCitySlide'));
+    expect(slides.length).toEqual(5);
+  });
+
+  // it('should have forecast template after triggering any city detail', () => {
+  //   tagEl = fixture.debugElement.query(By.css('.cardActionButton'));
+  //   tagEl.nativeElement.click();
+  //   expect(component.getCityDetail).toHaveBeenCalled();
+  // });
+
+  it('should have skeleton template before triggering any city detail', () => {
+    tagEl = fixture.debugElement.query(By.css('.actionSkeletonTemplate'));
+    expect(tagEl.nativeElement).toBeTruthy();
   });
 });
--- src/libs/shared-ui/search/search.component.spec.ts
+++ src/libs/shared-ui/search/search.component.spec.ts
@@ -1,5 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { IgxIconModule, IgxInputGroupModule } from 'igniteui-angular';
 
 import { SearchComponent } from './search.component';
@@ -7,6 +9,7 @@ import { SearchComponent } from './search.component';
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let tagEl: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -28,5 +31,28 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have search template after initialized', () => {
+    tagEl = fixture.debugElement.query(By.css('.actionSearchInput'));
+    expect(tagEl.nativeElement).toBeTruthy();
+  });
+
+  it('should have type in search input', () => {
+    tagEl = fixture.debugElement.query(By.css('.actionSearchInput'));
+    expect(tagEl.nativeElement).toBeTruthy();
+
+    tagEl.nativeElement.value = 'I am typing a city name';
+    tagEl.nativeElement.dispatchEvent(new Event('input'));
+    expect(tagEl.nativeElement.value).toBe('I am typing a city name');
+  });
+
+  it('should form controller set value changes the input value', () => {
+    tagEl = fixture.debugElement.query(By.css('.actionSearchInput'));
+    expect(tagEl.nativeElement).toBeTruthy();
+
+    component.createForm.controls.name.setValue('Random City Name');
+
+    expect(tagEl.nativeElement.value).toBe('Random City Name');
   });
 });
